06b01fbd7ba3587f20e91f42ec1a339ed4b7a42c
Derek Shao <shao.derek.now@gmail.com>
2018-12-01 17:56:51 -0500

    azure vm start up script

---
+++ scripts/vm-startup.sh
@@ -0,0 +1,6 @@
+# clone repository
+git clone https://github.com/Thomas-Leung/Pizza-Identifier.git
+
+# move the main application into XAMPP
+cd Pizza-Identifier
+sudo cp -r ./uploadfile /opt/lampp/htdocs/
