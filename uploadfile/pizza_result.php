a34be93ea38dab39637a3fde9dc03159b5718b3a
thomas <thomaselmoleung@gmail.com>
2018-12-01 15:47:47 -0500

    uploadfile code

---
+++ uploadfile/pizza_result.php
@@ -0,0 +1,13 @@
+<html>
+<body>
+  <?php
+  echo "<h1>Get image</h1>";
+  if (isset($_POST["pizzaImage"])) {
+    $image = $_POST["pizzaImage"];
+  } else{
+    echo "<p>cannot get the image</p>"
+  }?>
+
+  <?php <img ></image>
+</body>
+</html>
---
+++ uploadfile/uploadfile.php
@@ -0,0 +1,18 @@
+<!DOCTYPE>
+<html lane-"en">
+
+<head>
+  <meta charset-"utf-8"/ />
+  <title>Pizza Identifier</title>
+</head>
+
+<body>
+  <header>
+    <h3>Upload an image</h3>
+  </header>
+  <form method="post" action="pizza_result.php">
+    Select an image: <input type="file" name="pizzaImage"><br><br>
+    <input type="submit">
+  </form>
+</body>
+</html>
